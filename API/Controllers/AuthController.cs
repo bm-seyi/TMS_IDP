d62db6bed8a939244026e3e0cacadbebcbff4a77
bm-seyi <137287174+bm-seyi@users.noreply.github.com>
2025-04-26 13:09:22 +0100

    0.6.1

--- API/Controllers/AuthController.cs
+++ API/Controllers/AuthController.cs
@@ -3,21 +3,25 @@ using Microsoft.AspNetCore.Mvc;
 using SignInResult = Microsoft.AspNetCore.Identity.SignInResult;
 using TMS_IDP.DbContext;
 using TMS_IDP.Models.Controllers;
+using Duende.IdentityServer.Services;
 
 namespace TMS_IDP.Controllers
 {
     [Route("auth")]
-    public class AccountController : Controller
+    public class AuthController : Controller
     {
-        private readonly ILogger<AccountController> _logger;
+        private readonly ILogger<AuthController> _logger;
         private readonly SignInManager<ApplicationUser> _signInManager;
         private readonly UserManager<ApplicationUser> _userManager;
+        private readonly IIdentityServerInteractionService _interactionService;
 
-        public AccountController(ILogger<AccountController> logger, SignInManager<ApplicationUser> signInManager, UserManager<ApplicationUser> userManager)
+
+        public AuthController(ILogger<AuthController> logger, SignInManager<ApplicationUser> signInManager, UserManager<ApplicationUser> userManager, IIdentityServerInteractionService interactionService)
         {
             _logger = logger ?? throw new ArgumentNullException(nameof(logger));
             _signInManager = signInManager ?? throw new ArgumentNullException(nameof(signInManager));
             _userManager = userManager ?? throw new ArgumentNullException(nameof(userManager));
+            _interactionService = interactionService ?? throw new ArgumentNullException(nameof(interactionService));
         }
 
         [HttpGet("login")]
@@ -28,6 +32,7 @@ namespace TMS_IDP.Controllers
         }
 
         [HttpPost("login")]
+        [ValidateAntiForgeryToken]
         public async Task<IActionResult> Login([FromForm] LoginViewModel loginViewModel)
         {
             var user = await _userManager.FindByEmailAsync(loginViewModel.Email);
@@ -36,7 +41,13 @@ namespace TMS_IDP.Controllers
                 SignInResult result = await _signInManager.PasswordSignInAsync(user, loginViewModel.Password, false, false);
                 if (result.Succeeded)
                 {
-                    return Redirect(loginViewModel.ReturnUrl ?? "/");
+                    // Validate the return URL
+                    if (!Url.IsLocalUrl(loginViewModel.ReturnUrl) && !_interactionService.IsValidReturnUrl(loginViewModel.ReturnUrl))
+                    {
+                        return Redirect("~/");
+                    }
+                    
+                    return Redirect(loginViewModel.ReturnUrl ?? "~/");
                 }
             }
 
