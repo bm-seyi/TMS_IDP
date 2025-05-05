e40c9a808cf9778ee5367d25fce18feb64b4844b
bm-seyi <137287174+bm-seyi@users.noreply.github.com>
2025-05-05 15:42:07 +0100

    0.7.5

--- App/Models/IdentityServerModel.cs
+++ App/Models/IdentityServerModel.cs
@@ -1,14 +1,13 @@
-
-using Duende.IdentityServer.EntityFramework.Entities;
+using System.Text.Json.Serialization;
 
 namespace TMS_SEED.Models
 {
     public class IdentityServerModel
     {
-        public List<ClientModel> Clients { get; set; } = new List<ClientModel>();
-        public List<ApiScopeModel> ApiScopes { get; set; } = new List<ApiScopeModel>();
-        public List<IdentityResourceModel> IdentityResources { get; set; } = new List<IdentityResourceModel>();
-        public List<ApiResourceModel> ApiResources { get; set; } = new List<ApiResourceModel>();
+        public required List<ClientModel> Clients { get; set; }
+        public required List<ApiScopeModel> ApiScopes { get; set; }
+        public required List<IdentityResourceModel> IdentityResources { get; set; }
+        public required List<ApiResourceModel> ApiResources { get; set; }
 
     }
 
@@ -19,7 +18,7 @@ namespace TMS_SEED.Models
         public bool RequireClientSecret { get; set; }
         public required List<string> RedirectUris { get; set; }
         public required List<string> PostLogoutRedirectUris { get; set; }
-        public required List<string> AllowedCorsOrigins { get; set; }
+        public List<string> AllowedCorsOrigins { get; set; } = new List<string>();
         public required List<string> AllowedScopes { get; set; }
         public bool AllowOfflineAccess { get; set; }
         public required List<string> AllowedGrantTypes { get; set; }
@@ -44,10 +43,10 @@ namespace TMS_SEED.Models
     public class IdentityResourceModel
     {
         public required string Name { get; set; }
-        public required string DisplayName { get; set; }
-        public required string Description { get; set; }
+        public string? DisplayName { get; set; }
+        public string? Description { get; set; }
         public bool Required { get; set; }
-        public required List<string> UserClaims { get; set; }
+        public List<string>? UserClaims { get; set; }
     }
 
     public class ApiResourceModel
--- App/Utilities/Seeder.cs
+++ App/Utilities/Seeder.cs
@@ -1,5 +1,6 @@
 using Duende.IdentityServer.EntityFramework.DbContexts;
 using Duende.IdentityServer.EntityFramework.Entities;
+using Microsoft.EntityFrameworkCore;
 using TMS_SEED.Models;
 
 namespace TMS_SEED.Utilities
@@ -22,91 +23,91 @@ namespace TMS_SEED.Utilities
 
         public async Task SeedAsync()
         {
-
-            if (await _context.Database.EnsureCreatedAsync())
+            await _context.Database.EnsureCreatedAsync();
+            if (_context.Database.IsRelational()) 
             {
-
-                if (!_context.Clients.Any())
-                {
-                    foreach (ClientModel client in _identityServer.Clients)
-                    {
-                        var clientEntity = new Client
-                        {
-                            ClientId = client.ClientId,
-                            ClientName = client.ClientName,
-                            RequireClientSecret = client.RequireClientSecret,
-                            RedirectUris = client.RedirectUris.Select(r => new ClientRedirectUri { RedirectUri = r }).ToList(),
-                            PostLogoutRedirectUris = client.PostLogoutRedirectUris.Select(r => new ClientPostLogoutRedirectUri { PostLogoutRedirectUri = r }).ToList(),
-                            AllowedCorsOrigins = client.AllowedCorsOrigins.Select(o => new ClientCorsOrigin { Origin = o }).ToList(),
-                            AllowedScopes = client.AllowedScopes.Select(s => new ClientScope { Scope = s }).ToList(),
-                            AllowOfflineAccess = client.AllowOfflineAccess,
-                            AllowedGrantTypes = client.AllowedGrantTypes.Select(g => new ClientGrantType { GrantType = g }).ToList(),
-                            RequirePkce = client.RequirePkce,
-                            RefreshTokenUsage = client.RefreshTokenUsage,
-                            RefreshTokenExpiration = client.RefreshTokenExpiration,
-                            AccessTokenLifetime = client.AccessTokenLifetime,
-                            SlidingRefreshTokenLifetime = client.SlidingRefreshTokenLifetime,
-                            AllowAccessTokensViaBrowser = client.AllowAccessTokensViaBrowser,
-                            IdentityTokenLifetime = client.IdentityTokenLifetime
-                        };
-
-                        await _context.Clients.AddAsync(clientEntity);
-                    }
-                }
-
-                if (!_context.ApiScopes.Any())
-                {
-                    foreach (ApiScopeModel apiScope in _identityServer.ApiScopes)
-                    {
-                        var apiScopeEntity = new ApiScope
-                        {
-                            Name = apiScope.Name,
-                            DisplayName = apiScope.DisplayName,
-                            Description = apiScope.Description,
-                            Required = apiScope.Required,
-                            Emphasize = apiScope.Emphasize
-                        };
-
-                        await _context.ApiScopes.AddAsync(apiScopeEntity);
-                    }
-                }
-
-                if (!_context.IdentityResources.Any())
-                {
-                    foreach (IdentityResourceModel identityResource in _identityServer.IdentityResources)
-                    {
-                        var identityResourceEntity = new IdentityResource
-                        {
-                            Name = identityResource.Name,
-                            DisplayName = identityResource.DisplayName,
-                            Description = identityResource.Description,
-                            Required = identityResource.Required,
-                            UserClaims = identityResource.UserClaims.Select(c => new IdentityResourceClaim { Type = c }).ToList()
-                        };
-
-                        await _context.IdentityResources.AddAsync(identityResourceEntity);
-                    }
-                }
-
-                if (!_context.ApiResources.Any())
-                {
-                    foreach (ApiResourceModel apiResource in _identityServer.ApiResources)
-                    {
-                        var apiResourceEntity = new ApiResource
-                        {
-                            Name = apiResource.Name,
-                            DisplayName = apiResource.DisplayName,
-                            Description = apiResource.Description,
-                            Scopes = apiResource.Scopes.Select(s => new ApiResourceScope { Scope = s }).ToList(),
-                            UserClaims = apiResource.UserClaims.Select(c => new ApiResourceClaim { Type = c }).ToList()
-                        };
-
-                        await _context.ApiResources.AddAsync(apiResourceEntity);
-                    }
-                }
-
-                await _context.SaveChangesAsync();
+                await _context.Database.MigrateAsync();
             }
+            if (!_context.Clients.Any())
+            {
+                foreach (ClientModel client in _identityServer.Clients)
+                {
+                    Client clientEntity = new Client
+                    {
+                        ClientId = client.ClientId,
+                        ClientName = client.ClientName,
+                        RequireClientSecret = client.RequireClientSecret,
+                        RedirectUris = client.RedirectUris.Select(r => new ClientRedirectUri { RedirectUri = r }).ToList(),
+                        PostLogoutRedirectUris = client.PostLogoutRedirectUris.Select(r => new ClientPostLogoutRedirectUri { PostLogoutRedirectUri = r }).ToList(),
+                        AllowedCorsOrigins = client.AllowedCorsOrigins.Select(o => new ClientCorsOrigin { Origin = o }).ToList(),
+                        AllowedScopes = client.AllowedScopes.Select(s => new ClientScope { Scope = s }).ToList(),
+                        AllowOfflineAccess = client.AllowOfflineAccess,
+                        AllowedGrantTypes = client.AllowedGrantTypes.Select(g => new ClientGrantType { GrantType = g }).ToList(),
+                        RequirePkce = client.RequirePkce,
+                        RefreshTokenUsage = client.RefreshTokenUsage,
+                        RefreshTokenExpiration = client.RefreshTokenExpiration,
+                        AccessTokenLifetime = client.AccessTokenLifetime,
+                        SlidingRefreshTokenLifetime = client.SlidingRefreshTokenLifetime,
+                        AllowAccessTokensViaBrowser = client.AllowAccessTokensViaBrowser,
+                        IdentityTokenLifetime = client.IdentityTokenLifetime
+                    };
+                    await _context.Clients.AddAsync(clientEntity);
+                }
+            }
+
+            if (!_context.ApiScopes.Any())
+            {
+                foreach (ApiScopeModel apiScope in _identityServer.ApiScopes)
+                {
+                    var apiScopeEntity = new ApiScope
+                    {
+                        Name = apiScope.Name,
+                        DisplayName = apiScope.DisplayName,
+                        Description = apiScope.Description,
+                        Required = apiScope.Required,
+                        Emphasize = apiScope.Emphasize
+                    };
+
+                    await _context.ApiScopes.AddAsync(apiScopeEntity);
+                }
+            }
+
+            if (!_context.IdentityResources.Any())
+            {
+                foreach (IdentityResourceModel identityResource in _identityServer.IdentityResources)
+                {
+                    IdentityResource identityResourceEntity = new IdentityResource
+                    {
+                        Name = identityResource.Name,
+                        DisplayName = identityResource.DisplayName,
+                        Description = identityResource.Description,
+                        Required = identityResource.Required,
+                        UserClaims = identityResource?.UserClaims?.Select(c => new IdentityResourceClaim { Type = c }).ToList()
+                    };
+
+                    await _context.IdentityResources.AddAsync(identityResourceEntity);
+                }
+            }
+
+            if (!_context.ApiResources.Any())
+            {
+                foreach (ApiResourceModel apiResource in _identityServer.ApiResources)
+                {
+                    var apiResourceEntity = new ApiResource
+                    {
+                        Name = apiResource.Name,
+                        DisplayName = apiResource.DisplayName,
+                        Description = apiResource.Description,
+                        Scopes = apiResource.Scopes.Select(s => new ApiResourceScope { Scope = s }).ToList(),
+                        UserClaims = apiResource.UserClaims.Select(c => new ApiResourceClaim { Type = c }).ToList()
+                    };
+
+                    await _context.ApiResources.AddAsync(apiResourceEntity);
+                }
+            }
+
+            await _context.SaveChangesAsync();
+            
         }
     }
 }
